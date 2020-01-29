960ea88dbe692deba518c11b142d51e99da3738c
Zack <ishaqamincoop@icloud.com>
2020-01-29 10:44:10 +0000

    Initial Commit

---
+++ protocolsDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  protocolsDemo
+//
+//  Created by Ishaq Amin on 29/01/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
