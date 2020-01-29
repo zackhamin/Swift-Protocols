47c0fdba4a7c1e4222f0bae2e9d2150150da55a6
Zack <ishaqamincoop@icloud.com>
2020-01-29 10:53:23 +0000

    inheritance protocols

--- protocolsDemo/main.swift
+++ protocolsDemo/main.swift
@@ -6,3 +6,28 @@
 //  Copyright © 2020 Ishaq Amin. All rights reserved.
 //
 
+class Bird {
+    
+    var isFemale = true
+    
+    func layEgg(){
+        if isFemale{
+            print("The bird laid an egg")
+        }
+    }
+    
+    func fly(){
+        print("The bird flaps its wings and flys")
+    }
+}
+    
+class Eagle:Bird{
+    func soar(){
+        print("The eagle glides in the air using air currents")
+    }
+}
+
+
+let myEagle = Eagle()
+myEagle.fly()
+myEagle.layEgg()
